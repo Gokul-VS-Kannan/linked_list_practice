72165442cd6bab322e5315c62c1816c4142177cc
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2025-11-06 03:58:35 +0530

    a method to add new node to linked list

--- linked_list.py
+++ linked_list.py
@@ -10,3 +10,20 @@ class LinkedList :
     def __init__(self):
         self.head = None
 
+    # function to add node
+    def add(self,data):
+        new = Node(data)
+        # check if head node exist or not if no head node then make new node as head
+        if self.head is None:
+            self.head = new
+            return self.head
+        # if we have head node then make the new node as the tail
+        else:
+            current = self.head
+            # loop through the linked list untill the pointer reach none
+            while current.pointer is not None:
+                # make the current as next node
+                current = current.pointer
+            # once the pointer is none break the loop and link the new node now
+            current.pointer = new
+
