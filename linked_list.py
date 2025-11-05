98b1fc32cdcba6dbd2d90c65439c54072f483ecd
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2025-11-06 03:47:12 +0530

    created a linkedlist class and initialize head

--- linked_list.py
+++ linked_list.py
@@ -3,4 +3,10 @@ class Node :
     def __init__(self,data):
         self.data = data
         self.pointer = None
+    
+# creatring class for linked list
+
+class LinkedList :
+    def __init__(self):
+        self.head = None
 
