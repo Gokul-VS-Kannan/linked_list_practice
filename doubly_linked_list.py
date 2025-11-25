f2c7456b0758ba0ae5daf0675f1375681458ef7b
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2025-11-25 16:48:51 +0530

    created doubly linked list

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -3,3 +3,10 @@ class Node:
         self.data = data
         self.nref = None
         self.pref = None
+
+# creating class for doubly linked list
+class doubly_linked_list:
+    def __init__(self):
+        self.head = None
+
+    
