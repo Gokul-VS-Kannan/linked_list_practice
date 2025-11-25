96735912b23c84dcdcf079ed568fcc339c8d5dfc
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2025-11-25 16:54:58 +0530

    forward traversal method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -9,4 +9,13 @@ class doubly_linked_list:
     def __init__(self):
         self.head = None
 
-    
+    # forward traversal method
+    def show(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data,"--->", end=" ")
+                n = n.nref
+            print("None")
